030edc1d24d171715409112e70f654fd8bc170c6
AnimasianX <jlin2744@gmail.com>
2022-07-21 14:25:11 -0400

    added react-scripts

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,9 @@ const http = require('http');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const MONGO_URL = process.env.MONGO_URL;
 const { mongoConnect } = require('./services/mongo');
+
 
 const server = http.createServer(app);//express is basically a fancy listener function so any middleware, routes we add to it will essentially do the same thing. 
 //the added benefit of this is that now, we can separate our code and make it more modular and organized.
